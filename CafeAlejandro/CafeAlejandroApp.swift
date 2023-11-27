90207c1fa3aa1ced92fd8d9b77692343311a625a
Erik Arvedson <erik@arvedson.se>
2023-11-27 12:12:00 +0100

    Initial Commit

---
+++ CafeAlejandro/CafeAlejandroApp.swift
@@ -0,0 +1,17 @@
+//
+//  CafeAlejandroApp.swift
+//  CafeAlejandro
+//
+//  Created by Erik Arvedson on 2023-11-27.
+//
+
+import SwiftUI
+
+@main
+struct CafeAlejandroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
